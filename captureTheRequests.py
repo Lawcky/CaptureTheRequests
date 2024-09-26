2b0225a9e0d6a81228df08c060df16add379e97f
Lawcky <147432762+Lawcky@users.noreply.github.com>
2024-09-26 20:21:48 +0200

    Update captureTheRequests.py

--- captureTheRequests.py
+++ captureTheRequests.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # App Basic Customization :
 HOST = "0.0.0.0" # 0.0.0.0 to be accessible from everywhere
 PORT = 51951 # port on which the app will be running (for reverse proxy purposes)
-LOG_FILE_PATH = "requests.log" # files in which all connections will be stored
+LOG_FILE_PATH = "/listener/requests.log" # files in which all connections will be stored
 DEBUG = False # set false if production environnement
 HTTP_SERVER_DIRECTORY_PATH = "http" # directory in which files for the server will be stored (server files not app files)
 CUSTOM_HTTP_PATH = "" # leave empty if you want files to be directly accessible at the root / of the server
@@ -22,7 +22,7 @@ logging.basicConfig(level=logging.INFO)
 log_file = LOG_FILE_PATH
 # some logging magic
 file_handler = logging.FileHandler(log_file, mode='a') # append to the log_file
-file_handler.setLevel(logging.INFO)  
+file_handler.setLevel(logging.INFO)
 
 formatter = logging.Formatter('%(asctime)s - %(message)s') # set timestamps for each request
 file_handler.setFormatter(formatter)
@@ -37,9 +37,9 @@ def log_request():
         "-" * 47+"\n"+ #format
 
         f"\nRequest: {request.method} {request.url}\n"+
-        
+
         f"Headers: {request.headers}"+
-        
+
         f"POST Data: {request.data}\n"
     )
 
