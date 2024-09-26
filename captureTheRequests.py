178991213443f8f901b2a8e13221a1289118a80d
Lawcky <147432762+Lawcky@users.noreply.github.com>
2024-09-26 19:44:24 +0200

    Update captureTheRequests.py
    fixed deployement issues

--- captureTheRequests.py
+++ captureTheRequests.py
@@ -8,9 +8,10 @@ import os
 app = Flask(__name__)
 
 # App Basic Customization :
+HOST = "0.0.0.0" # 0.0.0.0 to be accessible from everywhere
 PORT = 51951 # port on which the app will be running (for reverse proxy purposes)
 LOG_FILE_PATH = "requests.log" # files in which all connections will be stored
-DEBUG = True # set false if production environnement
+DEBUG = False # set false if production environnement
 HTTP_SERVER_DIRECTORY_PATH = "http" # directory in which files for the server will be stored (server files not app files)
 CUSTOM_HTTP_PATH = "" # leave empty if you want files to be directly accessible at the root / of the server
 
@@ -33,15 +34,13 @@ app.logger.addHandler(file_handler)
 # logging function that'll log each and every request received on the endpoints
 def log_request():
     app.logger.info(
-        "-"*37+"\n\n"+ #formatting
-        
-        f"Request: {request.method} {request.url}\n"+
+        "-" * 47+"\n"+ #format
+
+        f"\nRequest: {request.method} {request.url}\n"+
         
         f"Headers: {request.headers}"+
         
-        f"POST Data: {request.data}\n\n"
-        
-        +"-"*63+"\n" #formatting
+        f"POST Data: {request.data}\n"
     )
 
 
@@ -63,7 +62,6 @@ def renfer_file(filename):
     base_directory = os.path.abspath(os.path.join(os.getcwd(), HTTP_SERVER_DIRECTORY_PATH))
     file_path = os.path.join(base_directory, filename)
 
-    print(f"DEBUGGING {file_path}")
     if os.path.exists(file_path):
         return send_file(file_path, as_attachment=False)
     else:
@@ -73,4 +71,4 @@ def renfer_file(filename):
 
 # run the app on the port
 if __name__ == '__main__':
-    app.run(port=PORT,debug=DEBUG)
+    app.run(host=HOST,port=PORT,debug=DEBUG)
